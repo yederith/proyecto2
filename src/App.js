2583c58dde21c84aef186c934c377781e4b731bd
yederith <70823913+yederith@users.noreply.github.com>
2020-09-06 16:50:42 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,41 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="container">
-      <h1 className="title">
-        Hello world <span aria-label="emoji" role="img">🔥</span>
-      </h1>
-     <input type="text" className="new-task"/>
-    </div>
-  );
-}
+import React, { Component } from "react";
+import "./App.css";
+import Title from './components/title';
+import Input from './components/input';
+import TaskList from './components/task-list';
+import Layout from './components/layout';
 
 class App extends Component {
-   constructor(props) {
-      super(props)
-      this.state = {
-         newTask: "",
-      }
-   }render() {
-      return (
-         <div className="container">
-            <h1 className="title">
-               Hello world
-               <span aria-label="emoji" role="img">🔥</span>
-            </h1>
-            <input type="text" className="new-task"/>
-         </div>
-      );
-   }
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      newTask: "",
+      tasks: []
+    };
+  }
 
-handleTaskChange = (event) => {
-   this.setState({
-      newTask: event.target.value,
-   })
+  handleTaskChange = event => {
+    this.setState({
+      newTask: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    let newTasks = this.state.tasks;
+    newTasks.push(this.state.newTask);
+    this.setState({
+      newTask: "",
+      tasks: newTasks
+    });
+  };
+
+  handleDelete = (id) => {
+    let newTasks = this.state.tasks;
+    newTasks.splice(id, 1)
+    this.setState({
+      tasks: newTasks,
+    })
+  }
+
+  render() {
+    return (
+      <Layout>
+        <Title/>
+        <Input 
+          handleSubmit={this.handleSubmit}
+          handleTaskChange={this.handleTaskChange}
+          value={this.state.newTask}
+        />
+        <TaskList 
+          tasks={this.state.tasks}
+          handleDelete={this.handleDelete}
+        />
+      </Layout>
+    );
+  }
 }
 
 export default App;
